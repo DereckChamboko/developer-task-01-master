4ea2315156692cd7e7f34199cfdbf92e0ee12807
DereckChamboko <52918869+DereckChamboko@users.noreply.github.com>
2021-08-15 21:23:20 +0200

    Unit tests

---
+++ electronic-payments-api/src/test/java/com/econetwireless/epay/business/config/EpayBusinessConfigTest.java
@@ -0,0 +1,21 @@
+package com.econetwireless.epay.business.config;
+
+import junit.framework.TestCase;
+
+public class EpayBusinessConfigTest extends TestCase {
+
+    public void testCreditsService() {
+    }
+
+    public void testTestCreditsService() {
+    }
+
+    public void testEnquiriesService() {
+    }
+
+    public void testReportingService() {
+    }
+
+    public void testPartnerCodeValidator() {
+    }
+}
